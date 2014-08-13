d4111b502141f28c077f1a7532dbbc878462110a
Sisyphos <sisyphos@josquin.org>
2014-08-13 19:33:41 +0200

    problem 21 #swip

--- Haskell/ProjectEuler/src/Util.hs
+++ Haskell/ProjectEuler/src/Util.hs
@@ -11,24 +11,21 @@ fibonacci :: [Integer]
 fibonacci = 0 : scanl (+) 1 fibonacci
 
 primeFactors :: Integer -> [Integer]
-primeFactors number = reverse $ primeFactors' number []  
-    where
-        primeFactors' n foundFactors = case found n of  
-            Nothing -> foundFactors
-            Just factor -> primeFactors' (n `div` factor) (factor : foundFactors)
-        found n = find (isDivisibleBy n) (primesUpTo n)
+primeFactors number = reverse $ primeFactors' number [] where
+    primeFactors' n foundFactors = case found n of  
+        Nothing -> foundFactors
+        Just factor -> primeFactors' (n `div` factor) (factor : foundFactors)
+    found n = find (isDivisibleBy n) (primesUpTo n)
 
 primesUpTo :: Integer -> [Integer]
 primesUpTo n = primes [2..n]
 
 primes :: [Integer] -> [Integer]
-primes numbers = sieve numbers Map.empty
-    where
-        sieve [] _ = []
-        sieve (x:xs) table = case Map.lookup x table of
-                Nothing -> x : sieve xs (Map.insert (x*x) [x] table)
-                Just facts -> sieve xs (List.foldl reinsert (Map.delete x table) facts)
-            where
+primes numbers = sieve numbers Map.empty where
+    sieve [] _ = []
+    sieve (x:xs) table = case Map.lookup x table of
+            Nothing -> x : sieve xs (Map.insert (x*x) [x] table)
+            Just facts -> sieve xs $ List.foldl reinsert (Map.delete x table) facts where
                 reinsert t prime = Map.insertWith (++) (x+prime) [prime] t
 
 isPrime :: Integer -> Bool
@@ -38,10 +35,10 @@ isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy number by = number `mod` by == 0 
 
 divisorCount :: Integer -> Integer
-divisorCount n = product summands
-    where
-        summands = List.map (\x -> toInteger (x+1)) primeFactorExponents
-        primeFactorExponents = List.map length $ group . primeFactors $ n
+divisorCount n = product summands where
+    summands = List.map addOne primeFactorExponents where
+        addOne x = toInteger $ x + 1
+    primeFactorExponents = List.map length $ group (primeFactors n)
         
 divisors :: Integer -> [Integer]
 divisors n = 1 : n : List.filter (isDivisibleBy n) [2..(n `div` 2)]
